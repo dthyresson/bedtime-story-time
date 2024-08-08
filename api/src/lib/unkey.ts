78f6c83412818e33caebf356f1434bc8847d3295
David Thyresson <dthyresson@gmail.com>
2024-08-08 11:59:55 -0400

    use unkey workspace keys

--- api/src/lib/unkey.ts
+++ api/src/lib/unkey.ts
@@ -1,7 +1,7 @@
 import { Ratelimit } from '@unkey/ratelimit'
 export const StoryWriterRatelimit = new Ratelimit({
   rootKey: process.env.UNKEY_ROOT_KEY,
-  namespace: 'bedtime-storytime',
+  namespace: process.env.UNKEY_NAMESPACE,
   limit: 20,
   duration: '1m',
   async: true,
