6fe4d3ba4af7670817d006b6effa92a852c027bf
David Thyresson <dthyresson@gmail.com>
2024-08-09 08:55:15 -0400

    ui

--- web/src/components/Spinner.tsx
+++ web/src/components/Spinner.tsx
@@ -1,7 +1,7 @@
 const Spinner = ({ caption }: { caption: string }) => (
   <div className="flex h-[60vh] flex-col items-center justify-center">
     <div className="flex flex-col items-center gap-4">
-      <p className="animate-pulse text-xl font-semibold text-gray-800">
+      <p className="animate-pulse text-2xl font-bold text-gray-800">
         {caption}
       </p>
     </div>
